f0433c30d76237278dbfe59f6c64558af29a8b33
NetoIQA75 <all-slavin@yandex.ru>
2023-11-30 11:52:17 +0300

    Using Constructors

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,22 +2,43 @@ public class Radio {
     private int currentNumberOfRadiostation; // текущий номер станции
     private int currentSoundVolume; // текущий уровень звука
 
+    private int countOfRadiostations; // количество радиостанций
+
+    public Radio(int countOfRadio) { // конструктор для установки количества радиостанций
+        this.countOfRadiostations = countOfRadio;
+    }
+
+    public Radio() {
+        this.countOfRadiostations = 10;// по умолчанию 10 радиостанций
+    }
+
+    public int getCountOfRadiostations(){
+        return countOfRadiostations;
+    }
+
+    public int getMaxNumberOfRadiostations(){ // геттер максимального номера станции при установке количества станций пользователем
+        return (countOfRadiostations - 1);
+    }
+
+
+
     public void setCurrentNumberOfRadiostation(int newCurrentNumberOfRadiostation) {
         if (newCurrentNumberOfRadiostation < 0) { // установка номера радиостанции меньше 0
             return;
         }
 
-        if (newCurrentNumberOfRadiostation > 9) { // установка номера радиостанции больше 9
+        if (newCurrentNumberOfRadiostation > countOfRadiostations -1) { // установка номера радиостанции больше максимальной
             return;
         }
         currentNumberOfRadiostation = newCurrentNumberOfRadiostation;
     }
+
     public int getCurrentNumberOfRadiostation() {
         return currentNumberOfRadiostation;
     }
 
     public void next() {
-        if (currentNumberOfRadiostation == 9) { // если текущий номер станции равен 9, то следующий будет 0
+        if (currentNumberOfRadiostation == countOfRadiostations - 1) { // если текущий номер станции равен максимальной (количество станций минус 1), то следующий номер будет 0
             currentNumberOfRadiostation = 0;
         } else {
             currentNumberOfRadiostation++; // иначе просто увеличиваем номер на 1
@@ -25,8 +46,8 @@ public class Radio {
     }
 
     public void prev() {
-        if (currentNumberOfRadiostation == 0) { // если текущий номер станции равен 0, то предыдущий будет 9
-            currentNumberOfRadiostation = 9;
+        if (currentNumberOfRadiostation == 0) { // если текущий номер станции равен 0, то предыдущий будет номер максимальной (количество станций минус 1)
+            currentNumberOfRadiostation = countOfRadiostations - 1;
         } else {
             currentNumberOfRadiostation--; // иначе уменьшаем на 1
         }
@@ -48,13 +69,13 @@ public class Radio {
         currentSoundVolume = newCurrentSoundVolume;
     }
 
-    public void increaseCurrentSoundVolume (){
+    public void increaseCurrentSoundVolume() {
         if (currentSoundVolume < 100) {
             currentSoundVolume++;
         }
     }
 
-    public void decreaseCurrentSoundVolume (){
+    public void decreaseCurrentSoundVolume() {
         if (currentSoundVolume > 0) {
             currentSoundVolume--;
         }
